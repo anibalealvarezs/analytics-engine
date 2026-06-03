2fa60ba56ef6c23045bece20dbb0727dc5e7878b
Anibal Alvarez <aalvarez@chmw.co>
2026-06-03 08:30:32 -0400

    chore: bind to port 8050 to resolve facade collision

--- main.py
+++ main.py
@@ -106,4 +106,4 @@ def calculate_regression(request: Request, payload: RegressionRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8050)
